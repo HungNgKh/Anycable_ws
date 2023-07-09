233bb2af8d65838f87929fde81bd94adff2fab49
HungNgKh <khanhhung.nguyen.dev@gmail.com>
2023-07-09 14:56:59 +0900

    :sparkles: setup connections

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    rescue_from StandardError, with: :report_error
+
+    private
+
+    def report_error(e)
+      Rails.logger.error e.message
+    end
   end
 end
